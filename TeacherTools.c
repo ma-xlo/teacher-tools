6023c52d5e82d65a827029f70f725ad6c993bf0e
ma-xlo <macorrea-@hotmail.com>
2023-02-28 01:56:59 -0300

    style: improved executable user interface

--- TeacherTools.c
+++ TeacherTools.c
@@ -1,27 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <locale.h>
 
 int main() {
 
+    setlocale(LC_ALL, "");
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    WORD saved_attributes;
+
+    /* Save current attributes */
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    saved_attributes = consoleInfo.wAttributes;
+
+    SetConsoleTextAttribute(hConsole, 13);
+    
     printf("   ______                __                 ______            __    \n");
     printf("  /_  __/__  ____ ______/ /_  ___  _____   /_  __/___  ____  / /____\n");
     printf("   / / / _ \\/ __ `/ ___/ __ \\/ _ \\/ ___/    / / / __ \\/ __ \\/ / ___/\n");
     printf("  / / /  __/ /_/ / /__/ / / /  __/ /       / / / /_/ / /_/ / (__  ) \n");
     printf(" /_/  \\___/\\__,_/\\___/_/ /_/\\___/_/       /_/  \\____/\\____/_/____/\n\n");
-    printf("  Checking updates... ");
-    system("git pull\n");
+
+    SetConsoleTextAttribute(hConsole, 15);
+
+    printf(" Checking updates... ");
+    system("git pull");
+
     printf("\n");
-    printf(" +-- Bem vindo(a) Teacher! -----------------------------------+\n");
-    printf(" |                                                            |\n");
-    printf(" | Servidor local da aplicacao inicializado com sucesso!      |\n");
-    printf(" |                                                            |\n");
-    printf(" | Endereco para acessa-lo: https://localhost:3000            |\n");
-    printf(" | Feche a janela para encerrar.                              |\n");
-    printf(" |                                                            |\n");
-    printf(" +------------------------------------------------------------+\n\n");
-    //getchar();
+    printf(" +-- ");
+    SetConsoleTextAttribute(hConsole, 3);
+    printf("Bem vinda, Teacher Lu!");
+    SetConsoleTextAttribute(hConsole, 15);
+    printf(" -------------------------------------+\n");
+    printf(" |                                                               |\n");
+    printf(" | O servidor local da aplicação foi inicializado com sucesso!   |\n");
+    printf(" |                                                               |\n");
+    printf(" | Endereço de acesso: https://localhost:3000                    |\n");
+    printf(" | Feche a janela para encerrar o servidor local.                |\n");
+    printf(" |                                                               |\n");
+    printf(" +---------------------------------------------------------------+\n\n");
+
+    SetConsoleTextAttribute(hConsole, saved_attributes);
 
     system("start chrome  http://localhost:3000/");
     system("node ./app.js");
+
     return 0; 
 }
