460392345c84ba277d36d699a7c63deda4527d35
macorrea <macorrea-@hotmail.com>
2025-08-02 09:45:04 -0300

    feat: add vercel server support

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import bodyParser from "body-parser";
-import generator from "./routes/generator.js";
-import registration from "./routes/registration.js";
+import generator from "../routes/generator.js";
+import registration from "../routes/registration.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,14 +12,16 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const publicPath = path.join(__dirname, "..", "public");
+
 app.use(bodyParser.json()); //Handles JSON requests
 app.use(bodyParser.urlencoded({ extended: true })); //Handles normal post requests
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(publicPath));
 app.use(generator);
 app.use(registration);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "html", "home.html"));
+  res.sendFile(path.join(publicPath, "html", "home.html"));
 });
 
 app.listen(port, () => {
